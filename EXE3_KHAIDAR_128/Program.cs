7c510ffb30f88bd5fff50ab35fa463c7984c9c66
TA1128 <m.khaidar.ft21@mail.umy.ac.id>
2022-11-23 16:43:13 +0700

    Node class represent the node of doubly linked list and checks if the list is empty

--- EXE3_KHAIDAR_128/Program.cs
+++ EXE3_KHAIDAR_128/Program.cs
@@ -1,1 +1,61 @@
-﻿
+﻿using System;
+using System.Runtime.Versioning;
+
+namespace DoublyLinkedList
+{
+    class Node
+    {
+        public int rollNumber;
+        public string name;
+        public Node next;
+        public Node prev;
+    }
+    class DoubleLinkedList
+    {
+        Node START;
+        public DoubleLinkedList()
+        {
+            START = null;
+        }
+        public void addNode()
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student: ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+
+            if (START == null || rollNo <= START.rollNumber)
+            {
+                if ((START != null) && (rollNo == START.rollNumber))
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+                newnode.next = START;
+                if (START != null)
+                    START.prev = newnode;
+                newnode.prev = null;
+                START = newnode;
+                return;
+            }
+            Node previous, current;
+            for (current = previous = START; current != null &&
+                rollNo >= current.rollNumber; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+            }
+            
+            }
+        }
+    }
+
+
